5658c4bc99eea9fc86b3f77f700e3d6f8237daaa
chubbymaus <court@loftydev.com>
2017-06-08 19:16:52 -0600

    Integrated new and create actions for portfolio

--- app/controllers/myworks_controller.rb
+++ app/controllers/myworks_controller.rb
@@ -1,5 +1,21 @@
 class MyworksController < ApplicationController
   def index
-    @portfolio_items = Mywork.all
+    @portfolio_item = Mywork.all
   end  
+
+  def new
+    @portfolio_item = Mywork.new 
+  end
+
+  def create
+    @portfolio_item = Mywork.new(params.require(:mywork).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to myworks_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
