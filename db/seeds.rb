533eb2bbbe604887b1a58c8039d90ba6ad492bfb
chubbymaus <court@loftydev.com>
2017-06-20 11:39:41 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,6 +45,11 @@ end
   )
 end
 
-
-
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Mywork.last.technologies.create!(
+      name: "Technology #{technology}"
+    )
+end
+puts "3 technologies created"  
