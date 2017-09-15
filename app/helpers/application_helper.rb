b9cefaf79fe16e61bd73f0cef212151e41bfb904
chubbymaus <court@loftydev.com>
2017-09-15 15:10:57 -0600

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,53 @@ module ApplicationHelper
   end 
 
   def copyright_generator
-    InfernoViewTool::Renderer.copyright 'Court Garr', 'All rights reserved'
+    InfernoViewTool::Renderer.copyright 'Court Garr', ''
+  end
+
+  def nav_items
+    [
+      {
+
+        url: root_path,
+        title: 'Home'
+      },
+      {
+
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+
+        url: myworks_path,
+        title: 'Portfolio'
+      },
+      {
+
+        url: contact_path,
+        title: 'Contact'
+      },
+
+    ]
   end  
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end  
+      
+
+    nav_links.html_safe
+  end  
+
+  def active? path
+    "active" if current_page? path
+  end  
+
 end
