7406211a6cbd1daa017c05e790724dc27ffc8d56
chubbymaus <court@loftydev.com>
2017-09-21 23:29:19 -0600

    implemented authorization rules for drag and drop

--- app/controllers/myworks_controller.rb
+++ app/controllers/myworks_controller.rb
@@ -1,7 +1,7 @@
 class MyworksController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout "myworks"
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   def index
     @portfolio_items = Mywork.by_position
   end  
