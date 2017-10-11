f647bb12c68ee408f9b60e2604164a34f5cdd697
chubbymaus <court@loftydev.com>
2017-10-11 04:43:07 -0600

    Built twitter parser for tweet links

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,7 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.page(params[:page]).per(5)
     @page_title = "Court Garr | Code & Design Blog"
+    @tweets = SocialTool.twitter_search
   end
 
   # GET /blogs/1
