6cd9a31b6132f2163c3239a3db0a663f0ae31b7c
chubbymaus <court@loftydev.com>
2017-10-27 17:48:13 -0600

    added courtgarr.com to allowed domains for cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://courtgarr-portfolio.herokuapp.com', 'http://courtgarr-portfolio.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://courtgarr-portfolio.herokuapp.com', 'http://courtgarr-portfolio.herokuapp.com', 'http://courtgarr.com']
   config.action_cable.url = "wss://courtgarr-portfolio.herokuapp.com/cable"
 end
